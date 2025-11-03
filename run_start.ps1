6b6db1b543afe29303765aa95c3a498ffd47c91c
Phuc Trinh <phuc.trinh@truecommerce.com>
2025-11-04 05:13:56 +0700

    Remove close win

--- run_start.ps1
+++ run_start.ps1
@@ -8,7 +8,7 @@ try {
     # Starrt Edqe
     Add-Content $logPath "$(Get-Date): Prepareparing to launch start.js"
     if (($null -ne $node) -and ($null -ne $script) -and (Test-Path $node) -and (Test-Path $script)) {
-        Start-Process -FilePath $node -ArgumentList $script
+        Start-Process -FilePath $node -ArgumentList $script -WindowStyle Hidden
         Add-Content $logPath "$(Get-Date): start.js launched successfully"
     } else {
         Add-Content $logPath "$(Get-Date): Node or script not found!"
@@ -18,18 +18,18 @@ try {
     Add-Content $logPath "$(Get-Date): Error - $_"
 }
 
-try {
-    # Start OOBE ---
-    Add-Content $logPath "$(Get-Date): Running AutoClickOOBE.ps1..."
-    Write-Host "Running AutoClickOOBE.ps1..."
-    Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File 'C:\automation\AutoClickOOBE.ps1'" -WindowStyle Hidden
-    Start-Sleep -Seconds 5
-    Write-Host "AutoClickOOBE.ps1 executed."
-    Add-Content $logPath "$(Get-Date): AutoClickOOBE.ps1 executed."
-}
-catch {
-    Add-Content $logPath "$(Get-Date): Error running AutoClickOOBE.ps1 - $_"
-    Write-Error "Error running AutoClickOOBE.ps1: $_"
-}    
+# try {
+#     # Start OOBE ---
+#     Add-Content $logPath "$(Get-Date): Running AutoClickOOBE.ps1..."
+#     Write-Host "Running AutoClickOOBE.ps1..."
+#     Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File 'C:\automation\AutoClickOOBE.ps1'" -WindowStyle Hidden
+#     Start-Sleep -Seconds 5
+#     Write-Host "AutoClickOOBE.ps1 executed."
+#     Add-Content $logPath "$(Get-Date): AutoClickOOBE.ps1 executed."
+# }
+# catch {
+#     Add-Content $logPath "$(Get-Date): Error running AutoClickOOBE.ps1 - $_"
+#     Write-Error "Error running AutoClickOOBE.ps1: $_"
+# }    
 
 Write-Host "=== [Auto Start Edge Script Completed] ==="
